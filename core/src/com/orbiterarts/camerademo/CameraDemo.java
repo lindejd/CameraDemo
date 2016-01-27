404d0f87c0fbe56206e6756dda54d9bb1909861f
jdl <derick.linde@gmail.com>
2016-01-26 23:36:40 -0500

    updating clear message

--- core/src/com/orbiterarts/camerademo/CameraDemo.java
+++ core/src/com/orbiterarts/camerademo/CameraDemo.java
@@ -55,7 +55,7 @@ public class CameraDemo extends ApplicationAdapter implements InputProcessor{
 
     @Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         camera.update();
