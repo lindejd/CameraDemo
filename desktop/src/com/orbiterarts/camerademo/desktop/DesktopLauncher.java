bce36b0a7fa1529498c4c873c1f3ee32517da83c
jdl <derick.linde@gmail.com>
2016-01-27 21:56:09 -0500

    changed desktop to 960x590

--- desktop/src/com/orbiterarts/camerademo/desktop/DesktopLauncher.java
+++ desktop/src/com/orbiterarts/camerademo/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import com.orbiterarts.camerademo.CameraDemo;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width=1024;
-		config.height=768;
+		config.width=960;
+		config.height=590;
 		new LwjglApplication(new CameraDemo(), config);
 	}
 }
